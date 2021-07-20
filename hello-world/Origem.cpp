601505ed9203a6231775aa74c65ec3a6ffd9fd6d
Pablo Porto <paulo.assis@ccc.ufcg.edu.br>
2021-07-20 14:45:25 -0300

    Lendo com erros

--- hello-world/Origem.cpp
+++ hello-world/Origem.cpp
@@ -21,9 +21,17 @@ int main() {
 
 	cout << "writing to disk" << endl;
 	ofstream fout("some-exemple.txt");
-	fout << "your name is: " << user.name << '\n' << "yor age is: " << user.age;
+	fout << user.name << " " << user.age;
 	fout.close();
 
+	cout << "reading from disk" << endl;
+	User user_from_written_file;
+	ifstream fin("some-exemple.txt");
+	fin >> user_from_written_file.name >> user_from_written_file.age;
+	fin.close();
+
+	cout << "showing on bash what was read" << endl;
+	cout << user_from_written_file.name << '\n' << user_from_written_file.age << endl;
 	return 0;
 
 };
